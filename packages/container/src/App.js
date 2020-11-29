b6ebcd408cff9c2daeec2ceeac640a37bd5f296a
Peter Siebold <peter.siebold@unitedplanet.com>
2020-11-29 15:20:06 +0100

    Remove unnecessary container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,6 @@ export default () => {
     return (
         <>
             <h1>Container App</h1>
-            <div>and here comes the marketing app</div>
             <MarketingApp />
         </>
     )
