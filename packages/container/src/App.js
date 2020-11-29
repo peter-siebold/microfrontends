371bed4d4e830ca0d5ceb7b9f25961acbf21ae20
Peter Siebold <peter.siebold@unitedplanet.com>
2020-11-29 14:04:20 +0100

    Added more descriptions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,6 +5,7 @@ export default () => {
     return (
         <>
             <h1>Container App</h1>
+            <div>and here comes the marketing app</div>
             <MarketingApp />
         </>
     )
