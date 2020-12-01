0ad49a388fb33f34c84d2854c26fb22808cb63a1
Peter Siebold <peter.siebold@unitedplanet.com>
2020-12-01 13:40:10 +0100

    Removed Whitespaces

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,6 @@
 import {mount} from "auth/AuthApp";
 import React, {useRef, useEffect} from "react";
 import { useHistory } from "react-router-dom"
-
-
 export default ({ onSignIn}) => {
     const ref = useRef(null);
     const history = useHistory();
@@ -12,7 +10,6 @@ export default ({ onSignIn}) => {
             initialPath: history.location.pathname,
             onNavigate: ({pathname: nextPathName}) => {
                 const {pathname} = history.location;
-
                 if(pathname !== nextPathName){
                     history.push(nextPathName);
                 }
